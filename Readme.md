eb3fb5766091a77768142bfc3efd889ba86ea5b0
Nathan Chell <zotecfury@gmail.com>
2023-02-14 01:28:18 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Simple program to determine the Arithmetic-Geometric mean of two positive real numbers.
+
+x <= G(x, y) <= A(x, y) <= y
+
+The geometric and arithmetic averages
+converge to the same value as precision goes to infinity.
+
+For a 'good enough' approximation, a precision of 5 is enough.
+
+Learn more: https://en.wikipedia.org/wiki/Arithmetic%E2%80%93geometric_mean
---
+++ src/AGM.c
@@ -0,0 +1,52 @@
+/*
+
+Arithmetric-geometric mean calculator
+for two positive real numbers
+
+x <= G(x, y) <= A(x, y) <= y
+
+The geometric and arithmetic averages
+converge to the same value as precision goes to infinity.
+
+For a 'good enough' approximation, a precision of 5 is enough.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+double ArithmeticMean(double a, double b){
+
+    return (a + b) / 2;
+}
+
+double GeometricMean(double a, double b){
+
+    return sqrt(a * b);
+}
+
+int main(){
+
+    double a, b, a1, b1, AGM;
+    int p, i;
+
+    printf("Enter two positive real numbers: ");
+    scanf("%lf %lf", &a, &b);
+    printf("Enter the precision: ");
+    scanf("%d", &p);
+
+    for(i = 0; i < p; i++){
+
+        a1 = ArithmeticMean(a, b);
+        b1 = GeometricMean(a, b);
+        a = a1;
+        b = b1;
+
+    }
+
+    AGM = ArithmeticMean(a, b);
+
+    printf("Arithmetic-geometric mean: %.17g", AGM);
+
+    return 0;
+
+}
