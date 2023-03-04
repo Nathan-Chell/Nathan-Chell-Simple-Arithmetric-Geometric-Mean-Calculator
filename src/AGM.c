7695e3c41dffa982a4b7811cb470b362d1e05183
Nathan Chell <zotecfury@gmail.com>
2023-03-04 03:05:06 +0000

    Made inputs clearer

--- src/AGM.c
+++ src/AGM.c
@@ -29,11 +29,12 @@ int main(){
     double a, b, a1, b1, AGM;
     int p, i;
 
-    printf("Enter two positive real numbers: ");
+    printf("Enter two positive real numbers (On the same line): ");
     scanf("%lf %lf", &a, &b);
     printf("Enter the precision: ");
     scanf("%d", &p);
 
+    /*Loop over the algorithm until the desired level of precision is achieved.*/
     for(i = 0; i < p; i++){
 
         a1 = ArithmeticMean(a, b);
